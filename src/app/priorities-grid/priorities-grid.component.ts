eb835898451dabf27e4d24f753f44943268f6909
Daniel Grady <danielgrady@dgrady-mbp.local>
2018-06-08 18:50:27 -0400

    cards implemented.  shared datapager.

--- src/app/priorities-grid/priorities-grid.component.ts
+++ src/app/priorities-grid/priorities-grid.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, ViewChild, HostListener, EventEmitter, Output, Input } from '@angular/core';
 import { TableItem, SampleData } from '../shared/sample-data';
-import { MatDialog, MatTable, MatSlideToggle, MatPaginator, MatTableDataSource } from '@angular/material';
+import { MatDialog, MatTable, MatSlideToggle, MatPaginator, MatTableDataSource, MatButtonToggleGroup } from '@angular/material';
 import { AddPrioirtyComponent } from '../add-prioirty/add-prioirty.component';
 
 
@@ -13,13 +13,14 @@ export class PrioritiesGridComponent implements OnInit {
   @Input() tableItems: TableItem[]
 
   @ViewChild(MatTable) table: MatTable<any>;
+  
   @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSlideToggle) modalSwitch: MatSlideToggle;
+  @ViewChild(MatButtonToggleGroup) group: MatButtonToggleGroup;
   @Output() createPriority: EventEmitter<null> = new EventEmitter<null>();
   @Output() editPriorityEvent: EventEmitter<TableItem> = new EventEmitter<TableItem>();
 
   dataSource: any;
-  
   innerWidth: number;
 
   displayedColumns = ['color', 'title', 'description', 'edit' ];
@@ -27,11 +28,17 @@ export class PrioritiesGridComponent implements OnInit {
   constructor(public dialog: MatDialog) { }
 
   ngOnInit() {
+    // monitor the width of the component
     this.innerWidth = window.innerWidth;
-    console.log(this.tableItems);
+
+    // set the button toggle group to default to table
+    this.group.value = 'table';
+
+    // set the datapager
     this.dataSource = new MatTableDataSource<TableItem>(this.tableItems);
-    console.log(this.dataSource);
     this.dataSource.paginator = this.paginator;
+
+    console.log(this.dataSource);
   }
 
   @HostListener('window:resize', ['$event'])
@@ -66,7 +73,7 @@ export class PrioritiesGridComponent implements OnInit {
 
     dialogRef.afterClosed().subscribe(result => {
       console.log('The dialog was closed');
-      if(result !== undefined) {
+      if (result !== undefined) {
         this.insertPriority(new TableItem(result.priorityTitle, result.priorityDescription, result.priorityInit));
       }
     });
